c99942bc2474d8c86ecfeb51ac7eb0121dd52752
sanket723 <sanketpawar723@gmail.com>
2023-03-31 15:29:46 +0530

    Reservation & Payment Controller/Service

--- src/main/java/com/driver/controllers/PaymentController.java
+++ src/main/java/com/driver/controllers/PaymentController.java
@@ -19,6 +19,15 @@ public class PaymentController {
         //If the amountSent is less than bill, throw "Insufficient Amount" exception, otherwise update payment attributes
         //If the mode contains a string other than "cash", "card", or "upi" (any character in uppercase or lowercase), throw "Payment mode not detected" exception.
         //Note that the reservationId always exists
-         return null;
+
+        Payment payment;
+        try{
+            payment = paymentService.pay(reservationId,amountSent,mode);
+        }
+        catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+
+         return payment;
     }
 }
--- src/main/java/com/driver/controllers/ReservationController.java
+++ src/main/java/com/driver/controllers/ReservationController.java
@@ -19,7 +19,14 @@ public class ReservationController {
         //Note that the vehicle can only be parked in a spot having a type equal to or larger than given vehicle
         //If parkingLot is not found, user is not found, or no spot is available, throw "Cannot make reservation" exception.
 
+        Reservation reservation;
+        try{
+            reservation = reservationService.reserveSpot(userId,parkingLotId,timeInHours,numberOfWheels);
+        }
+        catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
 
-        return null;
+        return reservation;
     }
 }
